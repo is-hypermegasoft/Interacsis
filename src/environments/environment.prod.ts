c8829a76eeb29edcf866aff12afb55edd926f883
Dhargelion <azmaeldc@gmail.com>
2019-05-03 10:13:03 -0400

    primer avance de home y firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  frebase:{
+    apiKey: "AIzaSyBkIJvFdEYcNOXSsMW1m4AtomrrC5f1ZV8",
+    authDomain: "hypermegasoft.firebaseapp.com",
+    databaseURL: "https://hypermegasoft.firebaseio.com",
+    projectId: "hypermegasoft",
+    storageBucket: "hypermegasoft.appspot.com",
+    messagingSenderId: "583072260319"
+
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBkIJvFdEYcNOXSsMW1m4AtomrrC5f1ZV8",
+    authDomain: "hypermegasoft.firebaseapp.com",
+    databaseURL: "https://hypermegasoft.firebaseio.com",
+    projectId: "hypermegasoft",
+    storageBucket: "hypermegasoft.appspot.com",
+    messagingSenderId: "583072260319"
+
+  }
 };
 
 /*
