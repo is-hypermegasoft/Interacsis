8605724b21f5651efe031a4354bc835930cb05ed
haroldvila007 <haroldvila007@gmail.com>
2019-06-07 16:53:05 -0400

    mejoras a la funcionalidad

--- src/app/components/lista/lista.component.ts
+++ src/app/components/lista/lista.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 import {ConexionService} from '../../services/conexion.service';
 
 @Component({
@@ -26,14 +26,77 @@ export class ListaComponent implements OnInit {
   ngOnInit() {
   }
 
-  /*eliminar(item){
-    this.conexion.eliminarItem(item);
+  //eliminar(item){
+    //this.conexion.eliminarItem(item);
+  //}
+  //editar(item){
+    //this.editarItem=item;
+  // }
+  //agregarItemEditado(){
+    //this.conexion.EditarItem(this.editarItem);
+  //}
+
+}*/
+import { Component, OnInit } from '@angular/core';
+import { ConexionService } from 'src/app/services/conexion.service';
+import { Observable } from 'rxjs';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+
+export interface Item 
+{ 
+  name: string; 
+}
+
+@Component({
+  selector: 'app-lista',
+  templateUrl: './lista.component.html',
+  styleUrls: ['./lista.component.css']
+})
+export class ListaComponent implements OnInit 
+{
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<any[]>;
+  //products: Observable<any[]>;
+  constructor(private afs: AngularFirestore, private service: ConexionService) 
+  {
+    //this.products = db.collection('products').valueChanges();
+    this.itemsCollection = afs.collection<Item>('products');
+    //con esto estamos guardando todo lo que este dentro la coleccion
+    this.items = this.itemsCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Item;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
   }
-  editar(item){
-    this.editarItem=item;
+
+  ngOnInit() {
   }
-  agregarItemEditado(){
-    this.conexion.EditarItem(this.editarItem);
-  }*/
+  eliminar(product)
+  {
+    const Swal = require('sweetalert2')
+    Swal.fire({
+      title: 'Esta seguro de Borrar este Producto?',
+      text: "porque  se Borraran de la base de datos!",
+      type: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Aceptar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.value) {
+      this.service.eliminarItemCariito(product);
+      Swal.fire(
+        'Eliminación Exitosa!  ',
+        'Muy Bien',
+        'success'
+        )
+      }
+    })
+  }
 
 }
+
--- src/app/services/conexion.service.ts
+++ src/app/services/conexion.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+/*import { Injectable } from '@angular/core';
 import {AngularFirestore,AngularFirestoreCollection,AngularFirestoreDocument} from '@angular/fire/firestore';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
@@ -36,14 +36,104 @@ export class ConexionService {
   agregarItem(item:Product2){
     this.products2Collection.add(item);
   }
- /* eliminarItem(item){
-    this.product2Doc=this.afs.doc<Product2>(`products2/${this.products2.id}`);
+ //* eliminarItem(item){
+    //this.product2Doc=this.afs.doc<Product2>(`products2/${this.products2.id}`);
+    //this.itemDoc.delete();
+  //}
+  //EditarItem(item){
+    //this.itemDoc=this.afs.doc<Product2>(`items/${item.id}`);
+  //  this.itemDoc.update(item);
+//  }
+
+
+}*/
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface Item 
+{ 
+  name: string; 
+  category: string;
+  price: string;
+  description: string;
+  image: string;
+  image1: string;
+  image2: string;
+  image3: string;
+  quantity: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConexionService 
+{
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  private itemsCollection2: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
+  products: Observable<Item[]>;
+  
+  private itemDoc: AngularFirestoreDocument<Item>;
+  private itemDoc2: AngularFirestoreDocument<Item>;
+
+  constructor(private afs: AngularFirestore) 
+  {
+    this.itemsCollection = afs.collection<Item>('items');
+    //con esto estamos guardando todo lo que este dentro la coleccion
+    this.items = this.itemsCollection.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Item;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
+    //codigo para agregar a carrito
+   this.itemsCollection2 = afs.collection<Item>('products');
+    this.products = this.itemsCollection2.snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Item;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
+  }
+
+  mostrarItem(item)
+  {
+    this.itemDoc = this.afs.doc<Item>(`items/${item.id}`);
+    //this.itemDoc.update(item);
+  }
+  ListaItem()
+  {
+    return this.items;
+  }
+
+  agregarItem(item: Item)
+  {
+    this.itemsCollection.add(item);
+  }
+
+  agregarCarrito(product: Item)
+  {
+    this.itemsCollection2.add(product);
+  }
+
+  eliminarItem(item)
+  {
+    this.itemDoc = this.afs.doc<Item>(`items/${item.id}`);
     this.itemDoc.delete();
   }
-  EditarItem(item){
-    this.itemDoc=this.afs.doc<Product2>(`items/${item.id}`);
+  eliminarItemCariito(product)
+  {
+    this.itemDoc2 = this.afs.doc<Item>(`products/${product.id}`);
+    this.itemDoc2.delete();
+  }
+  editarItem(item)
+  {
+    this.itemDoc = this.afs.doc<Item>(`items/${item.id}`);
     this.itemDoc.update(item);
-  }*/
-
-
+  }
 }
+
